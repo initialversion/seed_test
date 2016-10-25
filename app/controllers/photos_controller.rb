e18b9c90eeab8348752d061722a88fc62271682e
Murugan <murugu2012@gmail.com>
2016-10-25 19:46:05 +0530

    Only photo user can access show page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :current_user_must_be_photo_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_photo_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_photo_user
     photo = Photo.find(params[:id])
